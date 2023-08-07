72894decac4b712a98808065b0c62fea5445cf85
Giorgos Kalogeitonas <gkalog2@gmail.com>
2023-08-07 11:24:56 +0300

    add adminLTE theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sport Shoe Tracker</title>
-    <!-- Add your CSS stylesheets or links here if needed -->
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Sport Shoe Tracker</a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('sportshoes.index') }}">Sport Shoes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('sportshoes.add') }}">Add Shoes</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
-    <div class="container mt-4">
-        @yield('content')
-    </div>
+@extends('adminlte::page')
 
-    <!-- Add your JavaScript scripts or links here if needed -->
-</body>
-</html>
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to your AdminLTE-powered dashboard!</p>
+@stop
